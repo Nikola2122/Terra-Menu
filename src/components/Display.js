982595d4e607e15104d40a47795f868295bda6e9
Nikola Iliev <nikolailiev074@gmail.com>
2025-12-12 00:16:29 +0100

    Added formily and started with the search-feature

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,42 @@
 import Nothing from "./menus/Nothing";
 import Menu from "./menus/Menu";
 import DrinkMenu from "./menus/DrinkMenu";
+import { createForm } from '@formily/core'
+import { FormProvider, FormConsumer, Field } from '@formily/react'
+import {
+    FormItem,
+    FormLayout,
+    Input,
+    FormButtonGroup,
+    Submit,
+} from '@formily/antd'
+
+const form = createForm()
+
 
 function Display(props) {
+
     return (
         <div>
+            {props.whatToShow !== 'Nothing' && props.whatToShow !== 'Drinks' &&
+                (
+                    <FormProvider form={form}>
+                        <FormLayout layout="vertical">
+                            <Field
+                                name="input"
+                                title=""
+                                initialValue="Hello world"
+                                decorator={[FormItem]}
+                                component={[Input]}
+                            />
+                        </FormLayout>
+
+                        <FormButtonGroup>
+                            <Submit onSubmit={() => alert(form.values.input)}>submit</Submit>
+                        </FormButtonGroup>
+                    </FormProvider>
+                )
+            }
             {props.whatToShow === 'Nothing' ?
                 <Nothing/> : props.whatToShow === 'Drinks' ?
                     <div className={'menu special'}>
