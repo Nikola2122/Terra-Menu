714289765cefda73e5626dfa715a180d4ff4afb4
Nikola Iliev <nikolailiev074@gmail.com>
2026-03-10 22:32:14 +0100

    Added responsive snowflakes based on date

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ import {Snowfall} from "react-snowfall";
 
 function App() {
     const [lang, setLang] = useState('mk')
+    const today = new Date();
+    const month = today.getMonth() + 1; // JS months: 0=Jan, 11=Dec
+    const day = today.getDate();
+
+    const showSnow = (month === 11 && day >= 15)  // Nov 15+
+        || (month === 12)              // all December
+        || (month === 1)               // all January
+        || (month === 2 && day <= 15)  // Feb 1-15
+    ;
 
     return (
         <LangContext.Provider value={lang}>
-            <Snowfall
-                snowflakeCount={200}
-            />
+            {showSnow && <Snowfall snowflakeCount={200} />}
             <Router>
                 <div id={'wrapper'}>
                     <Header/>
